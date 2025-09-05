ec886d7464bc471845b52c049980138651a583b7
Annie <annmora21@gmail.com>
2025-09-04 22:44:13 -0700

    v-0.1

---
+++ l-3a.cpp
@@ -0,0 +1,17 @@
+// COMSC-210 | Lab 3A | Annie Morales
+// IDE used: Visual Studio Code
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Restaurant // Struct definition
+{
+    string name;
+    string location;
+    string address;
+    double price; 
+    double rating; // 0.0-5.0
+    bool isOpen;
+};
