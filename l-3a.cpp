b217106f983e96ea5a30cbe4b401a9b36482ff78
Annie <annmora21@gmail.com>
2025-09-04 23:06:04 -0700

    v-0.2

--- l-3a.cpp
+++ l-3a.cpp
@@ -3,15 +3,32 @@
 
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-struct Restaurant // Struct definition
+// Structure holding restaurant information
+struct Restaurant 
 {
     string name;
-    string location;
+    string cuisine;
     string address;
-    double price; 
+    double avgPrice; 
     double rating; // 0.0-5.0
     bool isOpen;
 };
+
+int main() 
+{
+    Restaurant myRestaurant = {"Pasta Palace", "Italian", "123 Main St", 15.99, 4.5, true};
+
+    // Display restaurant information
+    cout << endl;
+    cout << "Restaurant Name: " << myRestaurant.name << endl;
+    cout << "Cuisine Type: " << myRestaurant.cuisine << endl;
+    cout << "Address: " << myRestaurant.address << endl;
+    cout << "Average Price: $" << myRestaurant.avgPrice << endl;
+    cout << "Rating: " << myRestaurant.rating << "/5.0" << endl;
+    cout << "Open Now: " << (myRestaurant.isOpen ? "Yes" : "No") << endl;
+    cout << endl;
+
+    return 0;
+}
