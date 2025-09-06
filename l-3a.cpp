8f2b30b439c65871881ea7516a7cb00bee6d2c88
Annie <annmora21@gmail.com>
2025-09-06 14:51:09 -0700

    v-1.0

--- l-3a.cpp
+++ l-3a.cpp
@@ -16,27 +16,25 @@ struct Restaurant
 };
 
 Restaurant populateData();
+void outputData(Restaurant& temp);
 
 int main() 
 {
-    Restaurant temp;
-    temp = populateData();
-
+    Restaurant temp; 
+    temp = populateData(); // Calls the function to populate the struct
+    outputData(temp);
+    cout << "Thank you for using the restaurant review app!" << endl;
     cout << endl;
-    // Populate the input struct with user data
-    cout << "Restaurant name: " << temp.name << endl;
-    cout << "Cuisine type: " << temp.cuisine << endl;
-    cout << "Restaurant address: " << temp.restaurantAddress << endl;
-    cout << "Your rating: " << temp.rating << endl;
-    cout << "Your recommendation: " << temp.recommend << endl;
 
     return 0;
 }
 
-    // Function to get user input and populate the Restaurant struct
-    Restaurant populateData()
+// Function to get user input and populate the Restaurant struct
+Restaurant populateData()
 {
     Restaurant temp; // Declares an object named temp of type Restaurant
+    cout << endl;
+    cout << "Welcome to the restaurant review app!" << endl << endl;
     cout << "Enter restaurant name: ";
     getline(cin, temp.name);
 
@@ -50,6 +48,7 @@ int main()
     double userRating;
     cin >> userRating;
 
+    // Input validation for rating
     while (userRating < 0.0 || userRating > 5.0)
     {
         cout << "Error: Please enter a valid rating (0.0-5.0): ";
@@ -60,8 +59,9 @@ int main()
     cout << "Would you recommend this restaurant? (Y for Yes, N for No): ";
     char userAns;
     cin >> userAns;
-    userAns = toupper(userAns);
+    userAns = toupper(userAns); // Converts to uppercase to simplify validation
 
+    // Input validation for recommendation
     while (userAns != 'Y' && userAns != 'N')
     {
         cout << "Error: Please only enter Y for Yes or N for No: ";
@@ -70,5 +70,19 @@ int main()
     }
     temp.recommend = userAns;
     
-    return temp;
+    return temp; // Returns the populated struct
 };
+
+// Function to output the data stored in the Restaurant struct
+void outputData(Restaurant& temp)
+{
+    cout << endl << endl << endl << endl << endl << endl;
+    cout << "Here is your restaurant review!" << endl;
+    cout << "--------------------------------" << endl;
+    cout << "Restaurant name: " << temp.name << endl;
+    cout << "Cuisine type: " << temp.cuisine << endl;
+    cout << "Restaurant address: " << temp.restaurantAddress << endl;
+    cout << "Your rating: " << temp.rating << endl;
+    cout << "Recommend this Restaurant? " << (temp.recommend == 'Y' ? "Yes" : "No") << endl;
+    cout << endl;
+}
