fe24b2f8c5b085ffb95e8c68cad466412ac02726
Annie <annmora21@gmail.com>
2025-09-05 20:11:52 -0700

    v-0.5

--- l-3a.cpp
+++ l-3a.cpp
@@ -6,18 +6,37 @@
 using namespace std;
 
 // Structure holding restaurant information
-struct RestaurantData 
+struct Restaurant
 {
     string name;
     string cuisine;
     string restaurantAddress;
     double rating; // 0.0-5.0
-    bool review;
+    char recommend; // Y or N
 };
 
-// Function to get user input and populate the RestaurantData struct
-struct temp
+Restaurant populateData();
+
+int main() 
 {
+    Restaurant temp;
+    temp = populateData();
+
+    cout << endl;
+    // Populate the input struct with user data
+    cout << "Restaurant name: " << temp.name << endl;
+    cout << "Cuisine type: " << temp.cuisine << endl;
+    cout << "Restaurant address: " << temp.restaurantAddress << endl;
+    cout << "Your rating: " << temp.rating << endl;
+    cout << "Your recommendation: " << temp.recommend << endl;
+
+    return 0;
+}
+
+Restaurant populateData()
+{
+    // Function to get user input and populate the Restaurant struct
+    Restaurant temp;
     cout << "Enter restaurant name: ";
     getline(cin, temp.name);
     cout << "Enter cuisine type: ";
@@ -26,22 +45,8 @@ struct temp
     getline(cin, temp.restaurantAddress);
     cout << "Leave a rating. (0.0-5.0): ";
     cin >> temp.rating;
-    cout << "Would you like to leave a review? (1 for Yes, 0 for No): ";
-    cin >> temp.review;
+    cout << "Would you recommend this restaurant? (Y for Yes, N for No): ";
+    cin >> temp.recommend;
+
+    return temp;
 };
-
-
-int main() 
-{
-    RestaurantData temp;
-    temp = RestaurantData();
-
-    // Populate the input struct with user data
-    cout << "Restaurant name: " << temp.name << endl;
-    cout << "Cuisine type: " << temp.cuisine << endl;
-    cout << "Restaurant address: " << temp.restaurantAddress << endl;
-    cout << "Your rating: " << temp.rating << endl;
-    cout << "Your review: " << temp.review << endl;
-
-    return 0;
-}
